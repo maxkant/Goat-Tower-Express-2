2caeaacc514ae0f9dd340045911d0e9847a06963
maxskant <maxskant@users.noreply.github.com>
2017-09-04 21:24:45 -0700

    Initial commit

---
+++ Assets/Assets/Scripts/GoatController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoatController : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject grass;
+	public Text countText;
+	private Rigidbody2D rigidBody;
+	public float speed;
+	private int grassCount = 0;
+	private int nGrass = 40;
+
+	private SpriteRenderer grassBounds;
+	private SpriteRenderer goatBounds;
+
+	float verticalExtent;
+	float horizontalExtent;
+	float grassSizeX;
+	float grassSizeY;
+
+	void Start () {
+
+		grassBounds = grass.GetComponentInChildren<SpriteRenderer>();
+		goatBounds = this.gameObject.GetComponentInChildren<SpriteRenderer>();
+		rigidBody = GetComponent<Rigidbody2D>();
+
+		verticalExtent = Camera.main.orthographicSize;
+		horizontalExtent = verticalExtent * Screen.width / Screen.height;
+
+		grassSizeX = grassBounds.sprite.bounds.size.x;
+		grassSizeY = grassBounds.sprite.bounds.size.y;
+
+		for (int i = 0; i < nGrass; i++) {
+
+			GameObject g = Instantiate(grass);
+			float x = (1f - 2f * Random.value) * (horizontalExtent - grassSizeX / 2f);
+			float y = (1f - 2f * Random.value) * (verticalExtent - grassSizeY / 2f);
+
+			g.transform.Translate(new Vector3(x, y, 0));
+
+		}
+
+		float goatWidth = goatBounds.sprite.bounds.size.x;
+		float goatHeight = goatBounds.sprite.bounds.size.y;
+		
+	}
+		
+	// Update is called once per frame
+
+	void FixedUpdate () {
+		
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+		rigidBody.AddForce(movement * speed);
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		
+		if (other.gameObject.CompareTag("grass")) {
+
+			float x = (1f - 2f * Random.value) * (horizontalExtent - grassSizeX / 2f);
+			float y = (1f - 2f * Random.value) * (verticalExtent - grassSizeY / 2f);
+			other.transform.position = new Vector3(x, y, 0);
+			grassCount++;
+			countText.text = "You have eaten: " + grassCount.ToString() + " grass";
+
+		}
+
+	}
+
+}
---
+++ Assets/Assets/Scripts/GoatPenManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoatPenManager : MonoBehaviour {
+
+	public GameObject horizontalFence;
+	public GameObject verticalFence;
+	public int nHorizontal;
+	public int nVertical;
+	public float left;
+	public float top;
+	private SpriteRenderer verticalFenceBounds;
+	private SpriteRenderer horizontalFenceBounds;
+
+
+	// Use this for initialization
+	void Start () {
+
+		horizontalFenceBounds = horizontalFence.GetComponentInChildren<SpriteRenderer>();
+		verticalFenceBounds = verticalFence.GetComponentInChildren<SpriteRenderer>();
+
+		float hFenceWidth = horizontalFenceBounds.sprite.bounds.size.x;
+		float hFenceHeight = horizontalFenceBounds.sprite.bounds.size.y;
+
+		float vFenceWidth = verticalFenceBounds.sprite.bounds.size.x;
+		float vFenceHeight = verticalFenceBounds.sprite.bounds.size.y;
+
+
+		// Top and bottom fences
+
+		for (int i = 0; i < nHorizontal; i++){
+
+			GameObject hfTop = Instantiate(horizontalFence);
+			GameObject hfBottom = Instantiate(horizontalFence);
+
+			float x = i * hFenceWidth;
+
+			hfTop.transform.Translate(new Vector3(left, top, 0));
+			hfTop.transform.Translate(new Vector3(x, 0, 0));
+
+			hfBottom.transform.Translate(new Vector3(left, top, 0));
+			hfBottom.transform.Translate(new Vector3(x, -nVertical * vFenceHeight - hFenceHeight, 0));
+
+		}
+
+		// Left and right fences
+		for (int i = 0; i < nVertical; i++){
+
+			GameObject vfLeft = Instantiate(verticalFence);
+			GameObject vfRight = Instantiate(verticalFence);
+
+			float y =  - i * vFenceHeight - hFenceHeight;
+
+			vfLeft.transform.Translate(new Vector3(left, top, 0));
+			vfLeft.transform.Translate(new Vector3(0, y, 0));
+
+			vfRight.transform.Translate(new Vector3(left, top, 0));
+			vfRight.transform.Translate(new Vector3(nHorizontal * hFenceWidth - vFenceWidth, y, 0));
+				
+
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
